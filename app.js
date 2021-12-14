820bbf1a77aab4bed3a14d32ba98b6a2934c9406
Anurag Kumar <2002anuragksingh@gmail.com>
2021-12-15 01:26:36 +0530

    ready for host

--- app.js
+++ app.js
@@ -125,6 +125,6 @@ app.get("/users", function (req, res) {
     });
   });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server running on port 3000");
 });
